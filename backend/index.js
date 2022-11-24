0de6b4e48e7a6d81096d3ff70246ba10d75f0909
matthieu george <83599039+Dindonix@users.noreply.github.com>
2022-11-24 19:30:04 +0100

    Revert "Database"

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,14 @@
 require("dotenv").config();
 
-const cors = require("cors");
-const express = require("express");
-
-const app = express();
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
+const app = require("./src/app");
 
 const port = parseInt(process.env.APP_PORT ?? "5000", 10);
-const welcome = (req, res) => {
-  res.send("Welcome");
-};
 
-app.get("/", welcome);
-const takeItEasy = require("./TakeItEasy.jsx");
-
-app.get("/marco", takeItEasy.GetMarco);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
-    console.error(`Server is listening on ${port}`);
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`Server is listening on ${port}`);
   }
 });
