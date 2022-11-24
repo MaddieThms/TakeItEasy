08546cd1c84b646170ecf036bf1b0198bae02452
Javier Lopez <jvrlpzd@gmail.com>
2022-11-24 16:15:57 +0100

    Puzzle and citydata created, first trial version

---
+++ frontend/src/components/CityData.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function CityData({ place }) {
+  return (
+    <div>
+      <h3>{place.citation}</h3>
+      <h5>{place.histoire}</h5>
+      <a href={place.site}>Website</a>
+    </div>
+  );
+}
+
+export default Level;
