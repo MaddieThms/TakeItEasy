cf7b6b9f964dce9178cc9efe190dd58eaa161531
Javier Lopez <jvrlpzd@gmail.com>
2022-11-24 20:33:18 +0100

    Got backend back

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
 
-const app = require("./src/app");
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+const port = parseInt(process.env.APP_PORT ?? "5001", 10);
+const welcome = (req, res) => {
+  res.send("Welcome");
+};
+app.get("/", welcome);
+const takeItEasy = require("./TakeItEasy.jsx");
 
-const port = parseInt(process.env.APP_PORT ?? "5000", 10);
-
+app.get("/marco", takeItEasy.GetMarco);
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
-    // eslint-disable-next-line no-restricted-syntax
-    console.log(`Server is listening on ${port}`);
+    console.error(`Server is listening on ${port}`);
   }
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CityData from "./components/CityData";
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
-  const places = [
-    {
-      id: 1,
-      ville: "Paris",
-      histoire:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt velit molestiae libero quia in, sunt, nobis rerum quas saepe dolores enim dolore ad! Animi accusamus consequuntur distinctio necessitatibus vel.",
-      citation:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, inventore.",
-      site: "github.com",
-      img: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Canaletto_Entrance_to_the_Grand_Canal_Venice.jpg",
-      lvl: 1,
-    },
-    {
-      id: 2,
-      ville: "Lyon",
-      histoire:
-        "Lyon Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt velit molestiae libero quia in, sunt, nobis rerum quas saepe dolores enim dolore ad! Animi accusamus consequuntur distinctio necessitatibus vel.",
-      citation:
-        "Lyon Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, inventore.",
-      site: "google.com",
-      img: "http://www.rhone-alpes-info.com/files/2015/04/lyon.jpg",
-      lvl: 2,
-    },
-  ];
-
+  const [marco, setMarco] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5001/marco`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMarco(data);
+        console.warn(marco);
+      });
+  }, []);
   return (
     <div className="App">
-      <Home />
-      {places.map((place) => (
+      {marco.map((place) => (
         <CityData place={place} />
       ))}
-
-      <p>coucou</p>
     </div>
   );
 }
-
 export default App;
